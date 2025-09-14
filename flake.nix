7bb7823fad3f087859e8af14a09e076fffcbf212
Kalyan Mudumby <theinhumaneme@gmail.com>
2025-09-14 01:37:19 -0400

    nix: init dev environment

---
+++ flake.nix
@@ -0,0 +1,54 @@
+{
+  description = "nix-develop and code away";
+
+  inputs.nixpkgs = {
+    url = "https://flakehub.com/f/NixOS/nixpkgs/0.2505.809711";
+  };
+
+  outputs =
+    inputs:
+    let
+      supportedSystems = [
+        "x86_64-linux"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "aarch64-darwin"
+      ];
+      forEachSupportedSystem =
+        f:
+        inputs.nixpkgs.lib.genAttrs supportedSystems (
+          system:
+          f {
+            pkgs = import inputs.nixpkgs {
+              inherit system;
+              overlays = [ inputs.self.overlays.default ];
+            };
+          }
+        );
+    in
+    {
+      overlays.default = final: prev: rec {
+        nodejs = prev.nodejs;
+      };
+
+      devShells = forEachSupportedSystem (
+        { pkgs }:
+        {
+          default = pkgs.mkShell {
+            packages = with pkgs; [
+              # Add NodeJS Dependencies
+              node2nix
+              nodejs_24
+
+
+              # Add Build & DevEnv Dependencies
+              docker # Ensure that docker.service is running on your OS
+              docker-compose
+              docker-buildx
+              #
+            ];
+          };
+        }
+      );
+    };
+}
