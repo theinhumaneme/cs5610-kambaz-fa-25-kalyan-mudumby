c5864d35044ec87893fc5cfea0d28b021240dcf2
Kalyan Mudumby <theinhumaneme@gmail.com>
2025-09-17 20:49:50 -0400

    Assignment Editor: complete

--- app/Labs/page.tsx
+++ app/Labs/page.tsx
@@ -6,15 +6,23 @@ export default function Labs() {
       <ul>
         <li>
           <Link href="/Labs/Lab1" id="wd-lab1-link">
-            Lab 1: HTML Examples </Link>
+            Lab 1: HTML Examples{" "}
+          </Link>
         </li>
         <li>
           <Link href="/Labs/Lab2" id="wd-lab2-link">
-            Lab 2: CSS Basics </Link>
+            Lab 2: CSS Basics{" "}
+          </Link>
         </li>
         <li>
           <Link href="/Labs/Lab3" id="wd-lab3-link">
-            Lab 3: JavaScript Fundamentals </Link>
+            Lab 3: JavaScript Fundamentals{" "}
+          </Link>
+        </li>
+        <li>
+          <Link href="/" id="wd-lab3-link">
+            Kambaz{" "}
+          </Link>{" "}
         </li>
       </ul>
     </div>
