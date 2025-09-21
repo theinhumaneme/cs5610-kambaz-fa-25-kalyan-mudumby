6bbf6ab4c6d87fb1dfb41dad51abed3d3af898ca
Kalyan Mudumby <theinhumaneme@gmail.com>
2025-09-21 16:39:14 -0400

    update teslabot image

--- app/Labs/Lab1/page.tsx
+++ app/Labs/Lab1/page.tsx
@@ -125,7 +125,7 @@ export default function Lab1() {
         <br />
         Loading a local image:
         <br />
-        <img id="wd-teslabot" src="/images/teslabot.jpg" height="200px" />
+        <img id="wd-teslabot" src="/images/teslabot.png" height="200px" />
       </div>
       <div id="wd-forms">
         <h4>Form Elements</h4>
