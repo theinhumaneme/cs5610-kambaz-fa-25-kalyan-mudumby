2297d30fdb129c1f67c81d2a366814d55009897b
Kalyan Mudumby <theinhumaneme@gmail.com>
2025-09-21 16:39:56 -0400

    update course status sidebar

--- app/(Kambaz)/Courses/[cid]/Home/status.tsx
+++ app/(Kambaz)/Courses/[cid]/Home/status.tsx
@@ -2,9 +2,15 @@ export default function CourseStatus() {
   return (
     <div id="wd-course-status">
       <h2>Course Status</h2>
-      <button>Unpublish</button> <button>Publish</button>
-      {/* Complete on your own */}
-      <button>View Course Notifications</button>
+      <button>Unpublish</button>
+      <button>Publish</button>
+      <div><button>Import from Existing Content</button></div>
+      <div><button>Import from Commons</button></div>
+      <div><button>Choose Home Page</button></div>
+      <div><button>View Course Stream</button></div>
+      <div><button>View Announcement</button></div>
+      <div><button>New Analytics</button></div>
+      <div><button>View Course Notifications</button></div>
     </div>
   );
 }
