ad08ae94d6d4f765cd1242deb219363fe906115f
Kalyan Mudumby <theinhumaneme@gmail.com>
2025-09-21 16:39:28 -0400

    add more courses

--- app/(Kambaz)/Dashboard/page.tsx
+++ app/(Kambaz)/Dashboard/page.tsx
@@ -8,7 +8,7 @@ export default function Dashboard() {
       <div id="wd-dashboard-courses">
         <div className="wd-dashboard-course">
           <Link href="/Courses/1234" className="wd-dashboard-course-link">
-            <Image src="/images/reactjs.jpg" width={200} height={150} />
+            <Image src="/images/reactjs.webp" width={200} height={150} alt="reactjs" />
             <div>
               <h5> CS1234 React JS </h5>
               <p className="wd-dashboard-course-title">
@@ -18,8 +18,54 @@ export default function Dashboard() {
             </div>
           </Link>
         </div>
-        <div className="wd-dashboard-course"> ... </div>
-        <div className="wd-dashboard-course"> ... </div>
+        <div className="wd-dashboard-course">
+          <Link href="/Courses/4567" className="wd-dashboard-course-link">
+            <Image src="/images/vuejs.png" width={200} height={150} alt="reactjs" />
+            <div>
+              <h5> CS4567 Vue JS </h5>
+              <p className="wd-dashboard-course-title">
+                Vue.Js Developer
+              </p>
+              <button> Go </button>
+            </div>
+          </Link>
+        </div>
+        <div className="wd-dashboard-course">
+          <Link href="/Courses/8745" className="wd-dashboard-course-link">
+            <Image src="/images/solidjs.png" width={200} height={150} alt="reactjs" />
+            <div>
+              <h5> CS8745 Solid JS </h5>
+              <p className="wd-dashboard-course-title">
+                Solid.Js Developer
+              </p>
+              <button> Go </button>
+            </div>
+          </Link>
+        </div>
+        <div className="wd-dashboard-course">
+          <Link href="/Courses/9874" className="wd-dashboard-course-link">
+            <Image src="/images/solidjs.png" width={200} height={150} alt="reactjs" />
+            <div>
+              <h5> CS9874 Advanced Solid JS </h5>
+              <p className="wd-dashboard-course-title">
+                Senior Solid.Js Developer
+              </p>
+              <button> Go </button>
+            </div>
+          </Link>
+        </div>
+        <div className="wd-dashboard-course">
+          <Link href="/Courses/0258" className="wd-dashboard-course-link">
+            <Image src="/images/reactjs.webp" width={200} height={150} alt="reactjs" />
+            <div>
+              <h5> CS0258 React JS </h5>
+              <p className="wd-dashboard-course-title">
+                Senior ReactJS Developer
+              </p>
+              <button> Go </button>
+            </div>
+          </Link>
+        </div>
       </div>
     </div>
   );
