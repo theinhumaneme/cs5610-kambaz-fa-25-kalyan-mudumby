7cd6032d71eeeb9fd116ddd5da4605decb9cca17
Kalyan Mudumby <theinhumaneme@gmail.com>
2025-09-21 16:37:04 -0400

    add pages for empty links

---
+++ app/(Kambaz)/Courses/[cid]/Grades/page.tsx
@@ -0,0 +1,7 @@
+export default function Grades() {
+    return (
+        <div>
+            <h2>Grades</h2>
+        </div>
+    );
+}
---
+++ app/(Kambaz)/Inbox/page.tsx
@@ -0,0 +1,7 @@
+export default function Inbox() {
+    return (
+        <div>
+            <h2>Inbox</h2>
+        </div>
+    );
+}
