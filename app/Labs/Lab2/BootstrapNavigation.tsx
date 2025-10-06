c1a8861dd6e10167f9919c2d97ffa8fd2cb1e438
Kalyan Mudumby <theinhumaneme@gmail.com>
2025-10-06 14:17:48 -0400

    forms: add tags

--- app/Labs/Lab2/BootstrapNavigation.tsx
+++ app/Labs/Lab2/BootstrapNavigation.tsx
@@ -5,11 +5,33 @@ import {
   CardImg,
   CardText,
   CardTitle,
+  Nav,
+  NavItem,
+  NavLink,
 } from "react-bootstrap";
 
 export default function BootstrapNavigation() {
   return (
     <div>
+      <div id="wd-css-navigating-with-tabs">
+        <h2>Tabs</h2>
+        <Nav variant="tabs">
+          <NavItem>
+            <NavLink href="#/Labs/Lab2/Active">Active</NavLink>
+          </NavItem>
+          <NavItem>
+            <NavLink href="#/Labs/Lab2/Link1">Link 1</NavLink>
+          </NavItem>
+          <NavItem>
+            <NavLink href="#/Labs/Lab2/Link2">Link 2</NavLink>
+          </NavItem>
+          <NavItem>
+            <NavLink href="#/Labs/Lab2/Disabled" disabled>
+              Disabled
+            </NavLink>
+          </NavItem>
+        </Nav>
+      </div>
       <div id="wd-css-navigating-with-cards">
         <h2> Cards </h2>
         <Card style={{ width: "18rem" }}>
