5c27083b739d5cbe1ff2273bdd5040c9ef731e46
Kalyan Mudumby <theinhumaneme@gmail.com>
2025-10-06 18:18:54 -0400

    nix: add shellHook
    install and start the dev server, once you drop in the dev server.

--- flake.nix
+++ flake.nix
@@ -45,8 +45,16 @@
               #docker # Ensure that docker.service is running on your OS
               #docker-compose
               #docker-buildx
-              #
+              cowsay
+              lolcat
+
             ];
+            GREETING = "CS5610 Environment Activated!";
+
+            shellHook = ''
+              echo "Installing NPM Dependencies" | cowsay | lolcat && sleep 1 && npm install .
+              echo "Starting the Dev Server" | cowsay | lolcat && sleep 1 && npm run dev
+            '';
           };
         }
       );
