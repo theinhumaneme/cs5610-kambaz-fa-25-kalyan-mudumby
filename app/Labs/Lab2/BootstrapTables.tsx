3d101c4767c2dd528d6e0294651b174de54b1e10
Kalyan Mudumby <theinhumaneme@gmail.com>
2025-10-06 14:17:58 -0400

    fix forms not being responsive

--- app/Labs/Lab2/BootstrapTables.tsx
+++ app/Labs/Lab2/BootstrapTables.tsx
@@ -1,4 +1,4 @@
-import { Table } from "react-bootstrap";
+import { Container, Table } from "react-bootstrap";
 
 export default function BootstrapTables() {
   return (
@@ -42,46 +42,48 @@ export default function BootstrapTables() {
       </Table>
       <div id="wd-css-responsive-tables">
         <h2>Responsive tables</h2>
-        <Table responsive>
-          <thead>
-            <tr>
-              <th>Very</th>
-              <th>long</th>
-              <th>set</th>
-              <th>of</th>
-              <th>columns</th>
-              <th>Very</th>
-              <th>long</th>
-              <th>set</th>
-              <th>of</th>
-              <th>columns</th>
-              <th>Very</th>
-              <th>long</th>
-              <th>set</th>
-              <th>of</th>
-              <th>columns</th>
-            </tr>
-          </thead>
-          <tbody>
-            <tr>
-              <td>Very</td>
-              <td>long</td>
-              <td>set</td>
-              <td>of</td>
-              <td>columns</td>
-              <td>Very</td>
-              <td>long</td>
-              <td>set</td>
-              <td>of</td>
-              <td>columns</td>
-              <td>Very</td>
-              <td>long</td>
-              <td>set</td>
-              <td>of</td>
-              <td>columns</td>
-            </tr>
-          </tbody>
-        </Table>
+        <Container>
+          <Table responsive>
+            <thead>
+              <tr>
+                <th>Very</th>
+                <th>long</th>
+                <th>set</th>
+                <th>of</th>
+                <th>columns</th>
+                <th>Very</th>
+                <th>long</th>
+                <th>set</th>
+                <th>of</th>
+                <th>columns</th>
+                <th>Very</th>
+                <th>long</th>
+                <th>set</th>
+                <th>of</th>
+                <th>columns</th>
+              </tr>
+            </thead>
+            <tbody>
+              <tr>
+                <td>Very</td>
+                <td>long</td>
+                <td>set</td>
+                <td>of</td>
+                <td>columns</td>
+                <td>Very</td>
+                <td>long</td>
+                <td>set</td>
+                <td>of</td>
+                <td>columns</td>
+                <td>Very</td>
+                <td>long</td>
+                <td>set</td>
+                <td>of</td>
+                <td>columns</td>
+              </tr>
+            </tbody>
+          </Table>
+        </Container>
       </div>
     </div>
   );
