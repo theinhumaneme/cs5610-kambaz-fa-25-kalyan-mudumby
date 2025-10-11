af8789ec3dee3d05f7f356c365c987447e18b728
Kalyan Mudumby <theinhumaneme@gmail.com>
2025-10-11 13:21:58 -0400

    fixup : fix toolbar

--- app/(Kambaz)/Courses/[cid]/components/TopToolBar.tsx
+++ app/(Kambaz)/Courses/[cid]/components/TopToolBar.tsx
@@ -3,25 +3,22 @@ import { BsPlus } from "react-icons/bs";
 
 export default function TopToolBar() {
   return (
-    <Form>
-      <Row>
-        <Col xs={8} xxl={3}>
-          <Form.Control type="text" placeholder="🔍 Search for Assignments" />
-        </Col>
-        <Col xs={4} className="d-flex justify-content around">
-          <div>
-            <Button variant="secondary" className="p-1 m-1">
-              <BsPlus /> Group
-            </Button>
-          </div>
-          <div>
-            <Button variant="danger" className="p-1 m-1">
-              <BsPlus />
-              Assignment
-            </Button>
-          </div>
-        </Col>
-      </Row>
+    <Form className="d-flex justify-content-between align-items-center">
+      <div>
+        <Form.Control
+          type="text"
+          placeholder="🔍 Search for Assignments"
+          style={{ width: "250px" }}
+        />
+      </div>
+      <div>
+        <Button variant="secondary" className="p-1 m-1">
+          <BsPlus /> Group
+        </Button>
+        <Button variant="danger" className="p-1 m-1">
+          <BsPlus /> Assignment
+        </Button>
+      </div>
     </Form>
   );
 }
