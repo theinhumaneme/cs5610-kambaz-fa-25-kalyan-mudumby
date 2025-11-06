c233c285771caf9ea5b1140a8a92f9e4d8ef5d08
Kalyan Mudumby <theinhumaneme@gmail.com>
2025-11-06 18:01:53 -0500

    start lab5

--- app/Labs/TOC.tsx
+++ app/Labs/TOC.tsx
@@ -52,6 +52,15 @@ export default function TOC() {
         </NavLink>
       </NavItem>
       <NavItem>
+        <NavLink
+          href="/Labs/Lab5"
+          as={Link}
+          className={`nav-link ${pathname.endsWith("Lab5") ? "active" : ""}`}
+        >
+          Lab 5
+        </NavLink>
+      </NavItem>
+      <NavItem>
         <NavLink href="/" as={Link}>
           Kambaz
         </NavLink>
